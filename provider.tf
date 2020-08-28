64fd89b5d2f4a83050301033c39d9bc70f033f00
az900spallawkar <66231134+az900spallawkar@users.noreply.github.com>
2020-08-28 12:24:40 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -12,7 +12,6 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  - ami           = "ami-0a13d44dccf1f5cf6"
-  + ami           = "ami-0fc841be1f929d7d1"
+   ami           = "ami-0a13d44dccf1f5cf6"
   instance_type = "t2.micro"
 }
