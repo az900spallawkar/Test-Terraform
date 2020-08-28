7be97f19401d2172c1777d7276c0f144538b5986
az900spallawkar <66231134+az900spallawkar@users.noreply.github.com>
2020-08-28 10:17:27 +0100

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0a13d44dccf1f5cf6"
+  instance_type = "t2.micro"
+}
